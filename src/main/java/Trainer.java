f4014d705b29244bd095746999c0f8cb672ef97b
Akshay Narayan @ HP360 (MyOfficeThing) <okkhoy@gmail.com>
2023-02-03 10:10:51 +0800

    Add trainer class

---
+++ src/main/java/Trainer.java
@@ -0,0 +1,31 @@
+public class Trainer {
+    public static void main(String[] args) {
+        Duck d = new Duck();
+        getToSpeak(d);
+
+        Bird b = (Bird) d;  // upcasting
+        getToSpeak(b);
+
+        Animal a = (Animal) b; // upcasting
+        getToSpeak(a);
+
+        Duck d2 = (Duck) a; // downcasting
+        getToSpeak(d2);
+
+        train(new Duck());
+        // train(new Parrot());
+
+        Animal a2 = new Animal();
+        Bird b2 = new Bird();
+
+    }
+
+    private static void getToSpeak(Animal animal) {
+        System.out.println(animal.speak());
+    }
+
+    private static void train(Bird bird) {
+        Duck d = (Duck) bird;
+        d.swim();
+    }
+}
